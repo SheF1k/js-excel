2b5775f162cdd6983e6c4191afa2e5b3ae7521ed
Oleksandr Reida <kissiarule@gmail.com>
2020-05-14 14:22:10 +0300

    feature(webpack): enable hmr for css

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,17 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
